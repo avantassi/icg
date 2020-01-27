6af7d32a2fb214be2eb7abd8bfacf84fc622b49f
Екатерина Савина <esavina@bkcco.ru>
2020-01-27 16:56:49 +0300

    added mobile version

--- js/main.js
+++ js/main.js
@@ -7,8 +7,27 @@ $('.nav__link').on( 'click', function(){
             }, 500 // скорость прокрутки
         );
     }
+    // $('.menu__box').toggleClass('visibility');
     return false;
 });
+
+$('.gamburger').click(function(){
+  $('.menu').toggleClass('menu-active');
+  $('.header').toggleClass('fixed');
+});
+
+$('.menu a').click(function() {
+
+  $('.menu').removeClass('menu-active');
+
+});
+
+// $('#menu__toggle').on('click', function(){
+//   $('.menu__box').toggleClass('visibility');
+//   $('.header').toggleClass('fixed');
+//   $('.hero').toggleClass('padding');
+//   return false;
+// })
 
 var mySwiperFeed = new Swiper ('.swiper-container', {
     // Optional parameters
@@ -28,12 +47,23 @@ var mySwiperNews = new Swiper ('.swiper-container--news', {
     // Optional parameters
     loop: true,
     slidesPerView: 'auto',
-    spaceBetween: 300,
+    spaceBetween: 150,
     centeredSlides: true,
 
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next--news',
         prevEl: '.swiper-button-prev--news',
+    },
+  })
+
+var mySwiper = new Swiper ('.swiper-container--hero', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
     },
   })
